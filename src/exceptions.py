08f88723a5ee99957d24bc3b303520e39b00bae9
UtkarshPratapSingh7777 <utkarshpratap7777@gmail.com>
2026-08-18 23:54:05 +0530

    Resolved exception error

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,11 +1,30 @@
 from __future__ import annotations
 class DiarizationPipelineError(Exception):
+    pass
+
 class ConfigurationError(DiarizationPipelineError):
+    pass
+
 class AudioProcessingError(DiarizationPipelineError):
+    pass
+
 class UnsupportedAudioFormatError(AudioProcessingError):
+    pass
+
 class CorruptedAudioError(AudioProcessingError):
+    pass
+
 class AudioTooShortError(AudioProcessingError):
+    pass
+
 class VADProcessingError(DiarizationPipelineError):
+    pass
+
 class EmbeddingExtractionError(DiarizationPipelineError):
+    pass
+
 class ClusteringError(DiarizationPipelineError):
+    pass
+
 class PipelineError(DiarizationPipelineError):
+    pass
